a493837fbb05074e80383b1a38bd3ca63ee92f43
Leandro Linares <leandro.linares@mercadolibre.com>
2018-02-19 18:04:33 -0300

    Always prioritize visuals before processing on line tool

--- src/Workspace/tools/Line.js
+++ src/Workspace/tools/Line.js
@@ -6,9 +6,9 @@ let y0 = 0;
 let line = [];
 
 function start({ x, y, color, shadowLayer }) {
+  draw({ x, y, color, shadowLayer });
   x0 = x;
   y0 = y;
-  draw({ x, y, color, shadowLayer });
 }
 
 function doBresenham({ x, y, color, shadowLayer }) {
@@ -21,8 +21,8 @@ function doBresenham({ x, y, color, shadowLayer }) {
 }
 
 function end({ x, y, color, shadowLayer, selectedLayer }) {
-  shadowLayer.clear();
   line.forEach(point => draw({ x: point.x, y: point.y, color, selectedLayer }));
+  shadowLayer.clear();
   line = [];
 }
 
