b80011a9ab2e758384147a45de9de1511cf83a7b
Leandro Linares <leandro.linares@mercadolibre.com>
2018-02-23 17:57:15 -0300

    Delete log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
-import shortid from 'shortid';
-console.log(shortid.generate());
+
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
