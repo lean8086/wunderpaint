dd3b0abd8da5333ecc25ed7ebeecfd60e1dc1c6f
Leandro Linares <leandro.linares@mercadolibre.com>
2018-02-23 15:05:35 -0300

    Simplify App scaffolding

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import Header from './Header';
-import Workspace from './Workspace';
+import React, { Component } from 'react';
+import Toolbox from './Toolbox';
+import Canvas from './Canvas';
+import Pencil from './tools/Pencil';
 
-const App = ({ match }) => (
-  <div>
-    <h3>{match.params.id}</h3>
-    <Header />
-    <Workspace />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTool: Pencil,
+      color: '#000',
+      selectedLayer: null,
+      scale: 10,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.match.params.id}
+        <Canvas
+          {...this.state}
+        />
+        <Toolbox
+          // selectLayer={(layer) => this.selectLayer(layer)}
+          selectTool={(tool) => this.selectTool(tool)}
+          selectColor={(color) => this.selectColor(color)}
+        />
+      </div>
+    );
+  }
+
+  selectTool(tool) {
+    this.setState({ selectedTool: tool.default });
+  }
+
+  selectColor(color) {
+    this.setState({ color });
+  }
+
+  selectLayer(layer) {
+    this.setState({ selectedLayer: layer });
+  }
+}
 
 export default App;
