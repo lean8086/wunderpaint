523fb8a2cf0d53738c79002a9f7d6ec84b77c988
Leandro Linares <leandro.linares@mercadolibre.com>
2018-03-02 12:12:42 -0300

    Restore clear method for layers

--- src/App/Layer.js
+++ src/App/Layer.js
@@ -28,6 +28,10 @@ class Layer extends Component {
     this.ctx = this.canvas.getContext('2d');
   }
 
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
   render() {
     return (
       <canvas
