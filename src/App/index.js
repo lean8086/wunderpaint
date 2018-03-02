31b2c3ca64333623a2dcc7bfd9923dd634b7e5a0
Leandro Linares <leandro.linares@mercadolibre.com>
2018-03-02 11:24:20 -0300

    Add className to components

--- src/App/index.js
+++ src/App/index.js
@@ -4,7 +4,7 @@ import Toolbox from './Toolbox';
 import Workspace from './Workspace';
 
 const App = (props) => (
-  <div class="app">
+  <div className='app'>
     {props.match.params.id}
     <SideToolbox />
     <Workspace />
