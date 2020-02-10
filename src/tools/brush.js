35ba511d956997bba2ce38c5b32787b08fac4e51
lean8086 <lli@honeypot.io>
2020-02-10 23:35:03 +0100

    Apply bresenham to brush

--- src/tools/brush.js
+++ src/tools/brush.js
@@ -1,11 +1,29 @@
-function draw({ x, y, color }) {
+import { plotLine } from '../utils/bresenham.mjs';
+
+let x0;
+let y0;
+
+function start({ x, y, color }) {
   return function (ctx) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, 1, 1);
+    x0 = x;
+    y0 = y;
   };
-};
+}
+
+function move({ x, y, color }) {
+  return function (ctx) {
+    ctx.fillStyle = color;
+    for (const point of plotLine(x0, y0, x, y)) {
+      ctx.fillRect(point.x, point.y, 1, 1);
+    }
+    x0 = x;
+    y0 = y;
+  };
+}
 
 export default {
-  down: draw,
-  move: draw,
+  down: start,
+  move,
 };
--- src/tools/eraser.js
+++ src/tools/eraser.js
@@ -1,10 +1,27 @@
-function erase({ x, y }) {
+import { plotLine } from '../utils/bresenham.mjs';
+
+let x0;
+let y0;
+
+function start({ x, y }) {
   return function (ctx) {
     ctx.clearRect(x, y, 1, 1);
+    x0 = x;
+    y0 = y;
   };
-};
+}
+
+function move({ x, y }) {
+  return function (ctx) {
+    for (const point of plotLine(x0, y0, x, y)) {
+      ctx.clearRect(point.x, point.y, 1, 1);
+    }
+    x0 = x;
+    y0 = y;
+  };
+}
 
 export default {
-  down: erase,
-  move: erase,
+  down: start,
+  move,
 };
