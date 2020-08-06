a7178422568b42119641b26e6b772de220ef5f34
lean8086 <lli@honeypot.io>
2020-08-06 15:47:40 +0200

    Add shortcuts for tools

--- src/tool.mjs
+++ src/tool.mjs
@@ -5,7 +5,7 @@ class Tool extends HTMLElement {
     return this.getAttribute('type');
   }
 
-  onChange() {
+  select() {
     dispatch({ type: 'setSelectedTool', selectedTool: this.type });
   }
 
@@ -15,14 +15,23 @@ class Tool extends HTMLElement {
 
     const tool = node.querySelector('.tool');
     tool.setAttribute('for', `tool--${this.type}`);
+    tool.setAttribute('title', this.getAttribute('description'));
+    tool.setAttribute('data-shortcut', this.getAttribute('shortcut'));
     // TODO: use slot
     tool.innerHTML += this.innerHTML;
     this.innerHTML = '';
 
     const input = node.querySelector('input');
-    input.addEventListener('change', () => this.onChange());
+    input.addEventListener('change', () => this.select());
     input.checked = this.type === getState().selectedTool;
     input.id = `tool--${this.type}`;
+
+    document.addEventListener('keypress', (ev) => {
+      if (ev.key === this.getAttribute('shortcut')) {
+        this.select();
+        input.checked = true;
+      }
+    });
 
     this.appendChild(node);
   }
